d20ab808f1a0f8e70ff7a67b3985bd74880f45f3
Jesper Håsteen <>
2018-12-11 14:54:52 +0100

    Added api, conf and docker build.

---
+++ src/main/java/com/github/nalha/springbootdemo/SimpleRestApi.java
@@ -0,0 +1,14 @@
+package com.github.nalha.springbootdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleRestApi {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
